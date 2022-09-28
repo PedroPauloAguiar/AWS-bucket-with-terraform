a06cc62a4eef012f56c06c4a3fe8f44ef0224abd
Pedro Paulo Aguiar <114187874+PedroPauloAguiar@users.noreply.github.com>
2022-09-27 22:35:06 -0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.31.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+
+  default_tags {
+    tags = {
+      owner      = "pedroaguiar"
+      managed-by = "terraform"
+    }
+  }
+
+}
